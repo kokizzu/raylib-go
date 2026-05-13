fe9fb0ca496d3629739a6e5a90261828ae4a5523
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2026-05-13 15:00:16 +0200

    purego embedded shared lib: tidy up code

--- raylib/embed.go
+++ raylib/embed.go
@@ -20,9 +20,9 @@ import (
 var embeddedLicense []byte
 
 // extractLib extracts the embedded shared library and returns the path to it
-func extractLib() (libname string) {
+func extractLib() string {
 	if os.Getenv("RAYLIB_NO_EMBED") == "1" {
-		return
+		return ""
 	}
 
 	userCacheDir, err := os.UserCacheDir()
@@ -30,9 +30,9 @@ func extractLib() (libname string) {
 		panic(err)
 	}
 
-	// becomes "C:\Users\{username}\AppData\Local\github.com\gen2brain\raylib-go\"
-	// or "$HOME/.cache/github.com/gen2brain/raylib-go/"
-	outDir := filepath.Join(userCacheDir, "github.com", "gen2brain", "raylib-go")
+	// becomes "C:\Users\{username}\AppData\Local\github.com\gen2brain\raylib-go\6.0\"
+	// or "$HOME/.cache/github.com/gen2brain/raylib-go/6.0/"
+	outDir := filepath.Join(userCacheDir, "github.com", "gen2brain", "raylib-go", requiredVersion)
 
 	// ensure directory exists
 	if err := os.MkdirAll(outDir, 0755); err != nil {
@@ -41,55 +41,45 @@ func extractLib() (libname string) {
 
 	// write license file, if not already exists
 	licenseFile := filepath.Join(outDir, "LICENSE")
-	if _, err := os.Stat(licenseFile); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.WriteFile(licenseFile, embeddedLicense, 0644); err != nil {
-				panic(err)
-			}
+	if _, err := os.Stat(licenseFile); os.IsNotExist(err) {
+		if err := os.WriteFile(licenseFile, embeddedLicense, 0644); err != nil {
+			panic(err)
 		}
 	}
 
-	gz, err := gzip.NewReader(bytes.NewReader(embeddedLib))
+	gzipReader, err := gzip.NewReader(bytes.NewReader(embeddedLib))
 	if err != nil {
 		panic(err)
 	}
-	defer gz.Close()
+	defer gzipReader.Close()
 
-	tr := tar.NewReader(gz)
+	tarReader := tar.NewReader(gzipReader)
 
-	for {
-		hdr, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
+	// we do not iterate trough the .tar.gz, because it should only contain one file
+	tarHeader, err := tarReader.Next()
+	if err != nil {
+		panic(err)
+	}
+
+	// get the name of the file inside the .tar.gz (e.g. libraylib.so.6.0.0 or raylib.dll)
+	destLib := filepath.Join(outDir, tarHeader.Name)
+
+	// write library, if not already exists
+	if fileInfo, err := os.Stat(destLib); os.IsNotExist(err) {
+		if file, err := os.OpenFile(destLib, os.O_RDWR|os.O_CREATE, 0644); err == nil {
+			defer file.Close()
+			if _, err := io.Copy(file, tarReader); err != nil {
+				panic(err)
+			}
+			return destLib
+		} else {
 			panic(err)
 		}
-
-		// only one file expected
-		if hdr.Typeflag == tar.TypeReg {
-			destLib := filepath.Join(outDir, hdr.Name)
-
-			// write library only if file not already exists
-			if fileInfo, err := os.Stat(destLib); err != nil {
-				if os.IsNotExist(err) {
-					if file, err := os.OpenFile(destLib, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644); err == nil {
-						defer file.Close()
-						if _, err := io.Copy(file, tr); err != nil {
-							panic(err)
-						}
-						libname = destLib
-					} else {
-						panic(err)
-					}
-				}
-			} else {
-				if fileInfo != nil && !fileInfo.IsDir() {
-					libname = destLib
-				}
-			}
-			break
+	} else {
+		if fileInfo != nil && !fileInfo.IsDir() {
+			return destLib
 		}
 	}
-	return
+
+	return ""
 }
